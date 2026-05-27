1be8f0c5351472ea66242672d5df3363664609d7
OmololaEyamuku <lolaeyanuku@yahoo.com>
2026-05-27 20:48:39 +0100

    trigger frontend CI/CD pipeline test v7

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,4 +1,4 @@
-// starttech frontend - CI/CD pipeline test v6
+// starttech frontend - CI/CD pipeline test v7
 
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { useAuth } from '@/hooks/useAuth'
